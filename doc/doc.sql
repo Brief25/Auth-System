01d8760ccba15d56f5c79e1c62f4a7dc00363a4f
Brief25 <dev.virendrakhare@gmail.com>
2025-07-29 09:43:01 +0530

    Update doc.sql

--- doc/doc.sql
+++ doc/doc.sql
@@ -1,22 +1,49 @@
 DB
 1.
-CREATE TABLE users (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL,
-    email VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL,
-    password_hash VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
-    is_verified TINYINT(1) DEFAULT 0,
-    otp_code VARCHAR(6) COLLATE utf8mb4_general_ci DEFAULT NULL,
-    otp_expiry DATETIME DEFAULT NULL,
-    role ENUM('user', 'admin') COLLATE utf8mb4_general_ci DEFAULT 'user',
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    failed_attempts INT(11) DEFAULT 0,
-    last_failed_at DATETIME DEFAULT NULL,
-    locked_until DATETIME DEFAULT NULL,
-    status ENUM('active', 'blocked') COLLATE utf8mb4_general_ci DEFAULT 'active',
-    deleted_at DATETIME DEFAULT NULL,
-    is_deleted TINYINT(1) DEFAULT 0,
-    PRIMARY KEY (id),
-    UNIQUE KEY email_unique (email),
-    INDEX (email)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 29, 2025 at 06:09 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `auth-system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `is_verified` tinyint(1) DEFAULT 0,
+  `otp_code` varchar(6) DEFAULT NULL,
+  `otp_expiry` datetime DEFAULT NULL,
+  `role` enum('user','admin') DEFAULT 'user',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `failed_attempts` int(11) DEFAULT 0,
+  `last_failed_at` datetime DEFAULT NULL,
+  `locked_until` datetime DEFAULT NULL,
+  `status` enum('active','blocked') DEFAULT 'active',
+  `deleted_at` datetime DEFAULT NULL,
+  `is_deleted` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
